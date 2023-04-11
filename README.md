c8d8908bd9580be55c6f7ca9b5d65799ca3b8fb5
Carl_Javier <ccjavier87@gmail.com>
2023-04-11 01:00:16 -0700

    complete

--- README.md
+++ README.md
@@ -1,1 +1,36 @@
-# Phase-1-Project
+## Phase-1-Project
+
+# Some goals
+- Design and architect features across a frontend (I'm guessing the look and feel, and style of the html page)
+- Integrate JavaScript and an external API
+- Debug issues in small- to medium-sized projects
+- Build and iterate on a project MVP
+
+# App structure
+- Background related to Dad Jokes
+- Get a random joke. Especiallu when page first loads.
+- Have a random button that produces a random joke.
+- Have a search button exist.
+- Have users search for a joke.
+- Search button does something when mouse is hovering over the buttons.
+- Fetch a random joke if the search bar is empty.
+
+Background: url(https://source.unsplash.com/random/?father+child)
+- new background made on page refresh
+- background is fixed with details in style.css
+
+Fonts: href="https://fonts.googleapis.com/css2?family=Gloria+Hallelujah&family=Open+Sans&family=Permanent+Marker&display=swap"
+
+# JS functions
+
+- 
+- getData: fetches api data and turns into json. However, the api has a limit of 20 jokes to return per page.
+- allJokes: uses fetched data and iterates through it. There is a maximum of 20 jokes provided per page, therefore to get all jokes, we must iterate through each page starting with "page = 1" as the passed element. With each iteration We then push jokes in the page into the "jokeBox" array. jokeBox array is declared at the top of the script. Once all jokes have been collected, jokeBox will be our main joke source. This function is triggered when DOM is loaded. 
+- filterJokes: uses ".filter" to sort through jokeBox for jokes that include values that have been input into the search bar (i.e. pants). With the getJoke() function, we can get a random item from the filtered jokes array.
+    If the value of the input is blank or "", then all jokes will be searched through. A joke list is present to display other jokes available. If the list of jokes is greater than 5 (>5), then we display on 5 random jokes. If the list size is less than or equal to 5 (<=5>), then we display the list of jokes up to the length of the array. Each joke is given a new "li" and displayed.
+- getJoke: picks a random item from the array of jokes passed into it.
+
+# Event Listeners
+- randomButton: triggered with "click", and changes image when mouse hovers over it.
+- searchButton: triggered with "click" and runs filterJokes function where the search bar value/input is taken as the filter parameter. The button also changes opacity when mouse hovers over it.
+- search bar event listener for when the "Enter" key is pressed. This will trigger the searchButton function and produce a joke that includes the search value, or a random joke if the search value is blank
--- script.js
+++ script.js
@@ -1,123 +1,100 @@
-let jokeBox = [];
+const jokeBox = [];
+const page = 1;
+const searchTerm = "";
+const searchButton = document.querySelector(".searchButton");
+const randomButton = document.getElementById("randomButton");
+let searchedValue = document.querySelector("#searchBar").value;
+   
+document.addEventListener("DOMContentLoaded", () => {
+    allJokes(1);
+    getJoke(jokeBox);
+});
 
-let page = 1;
-
-let joke = {
-
-    allJokes: function (page) {
-        let searchTerm = "&page=" + page;
-        this.getData(searchTerm)
-        .then ((response) => response.json()) 
-        .then((data) => {
-          if (data.current_page <= data.total_pages) 
-            {
-              let jokeMap = data.results.map((jokes) => jokes.joke)
-              for (let item of jokeMap) 
-                {
-                  jokeBox.push(item)
-               
-                }
-              this.allJokes (page + 1)
-            }
-        })
-    },
-
-    filterJokes: function (searchTerm) {
-        joke.allJokes(1)
-        let filtered = jokeBox.filter(item => item.includes(`${searchTerm}`));
-        document.querySelector(".jokeList").innerHTML = '';
-        filtered.forEach((e) => {
-            document.querySelector(".jokeList").innerHTML = "";
-            const newP = document.createElement("p");
-            newP.innerText = e;
-            document.querySelector(".jokeList").appendChild(newP)
-        })
-        
-    },
-
-    getData: function (searchTerm) {
-        return fetch("https://icanhazdadjoke.com/search?term=" + searchTerm + ""
+function allJokes(page) {
+    fetch(`https://icanhazdadjoke.com/search?term=&page=${page}`
         , {
             headers: {
                 'Accept': 'application/json'
             }
         })
-    },
-
-    getJoke: function (searchTerm) {
-        this.getData(searchTerm)
         .then((response) => response.json())
-            .then((data) => {
-                let randNum = Math.floor(Math.random() * data.results.length);
-                return this.displayJoke(data.results[randNum])
-               
-            })
-               
-            },
-    
-
-    getRandom: function () {
-        fetch("https://icanhazdadjoke.com/",
-        {
-            headers: {
-                'Accept': 'application/json'
+        .then((data) => { 
+            if (data.current_page <= data.total_pages) {
+                data.results.map((item) => { jokeBox.push(item.joke) });
+                allJokes(page + 1)
             }
+            else { getJoke(jokeBox) }
         })
-            .then((response) => response.json())
-            .then((randJoke) => this.displayJoke(randJoke))
-            .catch((error) => console.log("error"))
-    },
-
-    searchJoke: function () {
-        
-        this.getJoke(document.querySelector(".searchBar").value);
-        this.filterJokes(document.querySelector(".searchBar").value);
-        document.querySelector(".searchBar").value = "";
-    },
-
-    displayJoke: function (data) {
-        const joke = data.joke;
-        document.querySelector(".theJoke").innerText = joke;
-    }
-
-
 };
 
-var randButt = document.querySelector("#randomButton")
-randButt.addEventListener("click", function () {
-    joke.getRandom();
+function filterJokes() {
+    searchedValue = document.querySelector("#searchBar").value;
+    const filtered = jokeBox.filter(item => item.includes(searchedValue));
+    document.querySelector(".theJoke").innerText = getJoke(filtered);
+
+    const jokeUl = document.querySelector('.jokeList');
+    jokeUl.innerHTML="";
+    if (filtered.length > 5) {
+        for (i=1; i<=5; i++) {
+            const jokeLi = document.createElement('li');
+            jokeLi.innerHTML = getJoke(filtered);
+            jokeUl.appendChild(jokeLi);
+        }
+    } else {
+        for (i=1; i<filtered.length; i++) {
+            const jokeLi = document.createElement('li');
+            jokeLi.innerHTML = getJoke(filtered);
+            jokeUl.appendChild(jokeLi);
+        }
+    }
+};
+
+function getJoke(jokeArray) {
+    let randNum = Math.floor(Math.random() * jokeArray.length);
+    return jokeArray[randNum];
+};
+
+
+randomButton.addEventListener("click", function () {
+    document.querySelector(".theJoke").innerText = getJoke(jokeBox);
+
 });
-randButt.addEventListener("mouseover", function () {
-    randButt.innerText = 'Random Dad Joke';
+
+randomButton.addEventListener("mouseover", function () {
+    randomButton.innerText = 'Random Dad Joke';
 });
 
-randButt.addEventListener("mouseout", function () {
-    randButt.innerHTML = '<svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 496 512" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path d="M248 8C111 8 0 119 0 256s111 248 248 248 248-111 248-248S385 8 248 8zm33.8 161.7l80-48c11.6-6.9 24 7.7 15.4 18L343.6 180l33.6 40.3c8.7 10.4-3.9 24.8-15.4 18l-80-48c-7.7-4.7-7.7-15.9 0-20.6zm-163-30c-8.6-10.3 3.8-24.9 15.4-18l80 48c7.8 4.7 7.8 15.9 0 20.6l-80 48c-11.5 6.8-24-7.6-15.4-18l33.6-40.3-33.6-40.3zM398.9 306C390 377 329.4 432 256 432h-16c-73.4 0-134-55-142.9-126-1.2-9.5 6.3-18 15.9-18h270c9.6 0 17.1 8.4 15.9 18z"></path></svg>';
+randomButton.addEventListener("mouseout", function () {
+    randomButton.innerHTML = '&#128512;';
 });
 
-var searchButt = document.querySelector(".searchButton");
 
-searchButt.addEventListener("click", function() {
-    joke.searchJoke();
+searchButton.addEventListener("click", function () {
 
-    if (document.querySelector(".searchBar").value === null || "") {
-        joke.getRandom();
-    }});
+    if (document.querySelector("#searchBar").value === "") {
+        document.querySelector('.jokeList').innerHTML = ""
+        document.querySelector(".theJoke").innerText = getJoke(jokeBox);
+    }
+    else {
+    filterJokes();
+    }
+});
 
-searchButt.addEventListener("mouseover", function () {
-    searchButt.setAttribute("style", "background-color:white;")
-}, false);
-searchButt.addEventListener("mouseout", function () {
-    searchButt.setAttribute("style", "background-color:#7c7c7c2b;")
-}, false);
+searchButton.addEventListener("mouseover", function () {
+    searchButton.setAttribute("style", "background-color:white;")
+});
+
+searchButton.addEventListener("mouseout", function () {
+    searchButton.setAttribute("style", "background-color:#7c7c7c2b;")
+});
 
 document.querySelector(".searchBar").addEventListener("keyup", function (e) {
 
     if (e.key === "Enter") {
         e.preventDefault()
-        document.querySelector(".searchButton").click();
+        searchButton.click();
     }
 });
 
-joke.getRandom();
+
 
